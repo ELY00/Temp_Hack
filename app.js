48581687dde02b2ea65138630e975fbdda161c15
ELY00 <70960599+ELY00@users.noreply.github.com>
2020-11-01 13:48:01 +0530

    Done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const request = require('request')
+const multer = require("multer");
 
 const MongoClient = require('mongodb').MongoClient
 
@@ -10,17 +11,30 @@ var db
 var user = null;
 
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/files/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({
+    storage: storage,
+});
+
 const uri = "mongodb+srv://Hack:test@database.y1m2l.mongodb.net/<dbname>?retryWrites=true&w=majority"
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err =>
 {
     db = client.db("test");
-
+    console.log("connected");
 });
 
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('listening on 5000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('listening on 3000')
 })
 
 app.set('view engine', 'ejs')
@@ -61,7 +75,7 @@ app.get('/about', (req, res) => {
     res.render("index");
 })
 
-app.get('/account', (req, res) => {
+/*app.get('/account', (req, res) => {
     if (!user) {
         res.redirect('/login')
         return console.log('Not logged in');
@@ -70,57 +84,75 @@ app.get('/account', (req, res) => {
         if (err) return console.log(err)
         // console.log(result)
         res.render("account", {
-            todos: result,
-            username: user,
+            name: user,
+            age: result,
+            blood: result,
+            contact: result,
+            address: result,
+            date: result,
         })
     })
-})
+})*/
 
 app.post('/signup', (req, res) => {
     db.collection('signup').save(req.body, (err, result) => {
         if (err) {
-            res.redirect('/signup')
+            res.redirect('/signup');
             console.log(err)
         }
         console.log('saved to database')
-        res.redirect('/login')
+        res.redirect('/login');
     })
 })
 
-    app.post('/logindata', (req, res) => {
-        var token = req.body['g-recaptcha-response'];
-        var secret_key = '6LfbfswZAAAAAH7Q9WY_1D475Kfdr55WZJArInKA';
-        const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}`
+app.post('/logindata', (req, res) =>
+{
+    db.collection('signup').findOne({ username: req.body.username, password: req.body.password }, (err, result) =>
+    {
+        if (err)
+        {
+            return console.log(err);
+        }
+        if (!result)
+        {
+            res.render('login', { x: 'Username or password fault' });
+            res.end();
+            return console.log('Username or password fault');
+        } else
+        {
+            user = req.body.username;
+            console.log(req.body.username + ' was loggedIn successfully!');
+            res.redirect('/account')
+            return res.send()
+        }
+    });
+});
 
-        request(url, function (error, response) {
-            if (error) {
-                console.log(error);
-                res.render('Login', { x: 'Error in captcha' });
-            }
-            console.log(JSON.parse(response.body))
-            if (JSON.parse(response.body).success) {
-                db.collection('signup').findOne({ username: req.body.username, password: req.body.password }, (err, result) => {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    if (!result) {
-                        res.render('Login', { x: 'Username or password fault' });
-                        res.end();
-                        return console.log('Username or password fault');
-                    } else {
-                        user = req.body.username;
-                        console.log(req.body.username + ' was loggedIn successfully!');
-                        res.redirect('/account')
-                        return res.send()
-                    }
-                });
-            } else {
-                res.render('Login', { x: 'Failed captcha' });
-                return console.log('Failed captcha');
-            }
-            // return res.send();
-        });
-
+/*app.post('/add', upload.any('itemfile'), (req, res) => {
+    var data = {}
+    if (!req.body.name) {
+        res.redirect('/account')
+        return console.log('Item not added')
+    } else
+        data['name'] = req.body.name
+        data['age'] = req.body.age
+        data['blood'] = req.body.blood
+        data['contact'] = req.body.contact
+        data['address'] = req.body.address
+        data['date'] = req.body.date
+    if (req.files.length != 0)
+        data['itemfile'] = req.files[0].originalname
+    console.log(data)
+    db.collection(user).insertMany(data, (err, result) => {
+        if (err) return console.log(err)
+        console.log('Item added successfully!')
+        res.redirect('/account')
     })
+})*/
+
+app.post('/logout', (req, res) => {
+    user = null
+    res.redirect('/login')
+})
 
 client.close();
